58035e4bd749dcfb8b7e2c784cc9b8b06d1bb595
Martyna <martyna.nieckarz98@gmail.com>
2019-04-19 03:37:19 +0100

    added getters and setters for them

--- java/src/ie/tudublin/Airplanes.java
+++ java/src/ie/tudublin/Airplanes.java
@@ -30,4 +30,102 @@ public class Airplanes
 
     }
 
+    /**
+     * @return the model
+     */
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * @param model the model to set
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    /**
+     * @return the capacity
+     */
+    public String getCapacity() {
+        return capacity;
+    }
+
+    /**
+     * @param capacity the capacity to set
+     */
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
+    /**
+     * @return the weight
+     */
+    public String getWeight() {
+        return weight;
+    }
+
+    /**
+     * @param weight the weight to set
+     */
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    /**
+     * @return the noOfPrvFlights
+     */
+    public String getNoOfPrvFlights() {
+        return noOfPrvFlights;
+    }
+
+    /**
+     * @param noOfPrvFlights the noOfPrvFlights to set
+     */
+    public void setNoOfPrvFlights(String noOfPrvFlights) {
+        this.noOfPrvFlights = noOfPrvFlights;
+    }
+
+    /**
+     * @return the safetyLevel
+     */
+    public String getSafetyLevel() {
+        return safetyLevel;
+    }
+
+    /**
+     * @param safetyLevel the safetyLevel to set
+     */
+    public void setSafetyLevel(String safetyLevel) {
+        this.safetyLevel = safetyLevel;
+    }
+
+    /**
+     * @return the manufactureYear
+     */
+    public String getManufactureYear() {
+        return manufactureYear;
+    }
+
+    /**
+     * @param manufactureYear the manufactureYear to set
+     */
+    public void setManufactureYear(String manufactureYear) {
+        this.manufactureYear = manufactureYear;
+    }
+
+    /**
+     * @return the airplaneSpeed
+     */
+    public String getAirplaneSpeed() {
+        return airplaneSpeed;
+    }
+
+    /**
+     * @param airplaneSpeed the airplaneSpeed to set
+     */
+    public void setAirplaneSpeed(String airplaneSpeed) {
+        this.airplaneSpeed = airplaneSpeed;
+    }
+
 }    
--- java/src/ie/tudublin/Pilots.java
+++ java/src/ie/tudublin/Pilots.java
@@ -26,4 +26,88 @@ public class Pilots
         this.yrsOfExperience = yrsOfExperience;
     }
 
+    /**
+     * @return the pilotName
+     */
+    public String getPilotName() {
+        return pilotName;
+    }
+
+    /**
+     * @param pilotName the pilotName to set
+     */
+    public void setPilotName(String pilotName) {
+        this.pilotName = pilotName;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the dob
+     */
+    public String getDob() {
+        return dob;
+    }
+
+    /**
+     * @param dob the dob to set
+     */
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    /**
+     * @return the age
+     */
+    public String getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the rank
+     */
+    public String getRank() {
+        return rank;
+    }
+
+    /**
+     * @param rank the rank to set
+     */
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    /**
+     * @return the yrsOfExperience
+     */
+    public String getYrsOfExperience() {
+        return yrsOfExperience;
+    }
+
+    /**
+     * @param yrsOfExperience the yrsOfExperience to set
+     */
+    public void setYrsOfExperience(String yrsOfExperience) {
+        this.yrsOfExperience = yrsOfExperience;
+    }
+
 }    
